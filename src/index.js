00bf3506fade6aa3f51edf6489ea74c16761eb20
Fabricio Bedin <fabricio-bedin@hotmail.com>
2017-12-20 20:59:58 -0200

    primeiro objeto adicionado e renderizado

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import App from './components/app';
-import reducers from './reducers';
+import SearchBar from './components/search_bar';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const API_KEY = 'AIzaSyBXNHjCiTlR6XGwrCBvkzxzMQSbjW-VC8g';
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+
+const App = () => {
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
+}
+
+
+ReactDOM.render(<App />, document.querySelector('.container'));
