fbed9f399c4272bf0bb4f6bd3ee3b621a7b97781
Fabricio Bedin <fabricio-bedin@hotmail.com>
2017-12-22 21:09:24 -0200

    working with states in react

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,7 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const SearchBar = () => {
-  return <input />
+class SearchBar extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = { term: '' };
+  }
+
+
+  render() {
+    return (
+      <div>
+        <input onChange={ event => this.setState({ term: event.target.value })} />
+        Value of the input: { this.state.term }
+      </div>
+    );
+  }
+
+
+
+  // render() {
+  //   return <input onChange={event => console.log(event.target.value)} />;
+  // }
+
+  // render() {
+  //   return <input onChange={this.onInputChange} />;
+  // }
+  //
+  // onInputChange(event) {
+  //   console.log(event.target.value);
+  // }
+
+
 }
 
 export default SearchBar;
